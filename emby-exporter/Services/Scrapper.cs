e821be67397f6b8757ac7b0b7a6ee77c3017c330
jorturfer <jorge_turrado@hotmail.es>
2021-07-06 22:43:44 +0200

    Adds some metrics

--- emby-exporter/Services/Scrapper.cs
+++ emby-exporter/Services/Scrapper.cs
@@ -1,6 +1,7 @@
 ﻿using emby_exporter.Entities;
 using Prometheus;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -15,6 +16,11 @@ namespace emby_exporter.Services
         });
 
         private readonly Gauge EmbyUsers = Metrics.CreateGauge("emby_users_total", "Emby Users");
+        private readonly Gauge EmbySessions = Metrics.CreateGauge("emby_video_sessions", "Emby Video Sessions", new GaugeConfiguration
+        {
+            LabelNames = new[] { "type" }
+        });
+        private readonly Gauge EmbyDevices = Metrics.CreateGauge("emby_devices_total", "Emby Devices");
         private readonly Gauge EmbyInfo = Metrics.CreateGauge("emby_info", "Emby Info", new GaugeConfiguration
         {
             LabelNames = new[] { "id","operating_system","server_name","version","wan_address" }
@@ -43,6 +49,21 @@ namespace emby_exporter.Services
                 content = await response.Content.ReadAsStringAsync();
                 var users = JsonSerializer.Deserialize<IList<UsersResponse>>(content);
                 EmbyUsers.IncTo(users.Count);
+
+                response = await _httpClient.GetAsync("/Devices");
+                content = await response.Content.ReadAsStringAsync();
+                var devices = JsonSerializer.Deserialize<DevicesResponse>(content);
+                EmbyDevices.IncTo(devices.TotalRecordCount);
+
+                response = await _httpClient.GetAsync("/Sessions");
+                content = await response.Content.ReadAsStringAsync();
+                var sessions = JsonSerializer.Deserialize<IList<SessionsResponse>>(content);
+                var videoSessions = sessions.Where(x=>x.NowPlayingItem is not null).ToList();
+                EmbySessions.WithLabels("direct-play").IncTo(videoSessions.Count(x => x.TranscodeType == TranscodeType.Direct));
+                EmbySessions.WithLabels("audio-transcode").IncTo(videoSessions.Count(x => x.TranscodeType == TranscodeType.Audio));
+                EmbySessions.WithLabels("video-transcode").IncTo(videoSessions.Count(x => x.TranscodeType == TranscodeType.Video));
+                EmbySessions.WithLabels("full-transcode").IncTo(videoSessions.Count(x => x.TranscodeType == TranscodeType.Full));
+
 
                 response = await _httpClient.GetAsync("/System/Info");
                 content = await response.Content.ReadAsStringAsync();
